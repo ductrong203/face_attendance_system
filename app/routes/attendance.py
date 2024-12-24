9fe1a0df75202dc5042a042dd98a01c244c5307e
ductrong <trongdeck@gmail.com>
2024-12-24 14:00:35 +0700

    statistic start_date end_date

--- app/routes/attendance.py
+++ app/routes/attendance.py
@@ -7,38 +7,28 @@ bp = Blueprint('attendance', __name__, url_prefix='/attendance')
 @bp.route('/employee/<int:id>', methods=['GET'])
 def get_employee_attendance(id):
     try:
-        day = request.args.get('day', None)
-        month = request.args.get('month', None)
-        quarter = request.args.get('quarter', None)
-        year = request.args.get('year', None)
-        return get_employee_attendance_summary(id, day, month, quarter, year)
-
+        start_date = request.args.get('start_date', None)
+        end_date = request.args.get('end_date', None)
+        return get_employee_attendance_summary(id, start_date, end_date)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-#admin
-
+# adminadmin
 @bp.route('/admin/<int:id>', methods=['GET'])
-@jwt_required() 
+@jwt_required()
 def get_admin_attendance_by_employee(id):
     try:
-        day = request.args.get('day', None)
-        month = request.args.get('month', None)
-        quarter = request.args.get('quarter', None)
-        year = request.args.get('year', None)
-        return  get_admin_attendance_summary(id, day, month, quarter, year)
-
+        start_date = request.args.get('start_date', None)
+        end_date = request.args.get('end_date', None)
+        return get_admin_attendance_summary(id, start_date, end_date)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
 @bp.route('/admin/summary', methods=['GET'])
-@jwt_required() 
+@jwt_required()
 def get_admin_summary():
     try:
-        day = request.args.get('day', None)
-        month = request.args.get('month', None)
-        quarter = request.args.get('quarter', None)
-        year = request.args.get('year', None)
-        return  get_admin_attendance_summary(day, month, quarter, year)
-
+        start_date = request.args.get('start_date', None)
+        end_date = request.args.get('end_date', None)
+        return get_admin_attendance_summary(None, start_date, end_date)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
--- app/services/attendance_service.py
+++ app/services/attendance_service.py
@@ -5,52 +5,35 @@ from flask_jwt_extended import get_jwt_identity
 from sqlalchemy import func, and_
 from datetime import datetime
 from app import db
-def get_employee_attendance_summary(employee_id, day=None, month=None, quarter=None, year=None):
+def get_employee_attendance_summary(employee_id, start_date=None, end_date=None):
     try:
         query = Attendance.query.filter(Attendance.id_employee == employee_id)
 
-        if day:
-            query = query.filter(func.DAY(Attendance.date) == int(day))
-        if month and year:
+        if start_date and end_date:
             query = query.filter(
-                and_(
-                    func.MONTH(Attendance.date) == int(month),
-                    func.YEAR(Attendance.date) == int(year)
-                )
+                Attendance.date.between(datetime.strptime(start_date, '%Y-%m-%d'), 
+                                        datetime.strptime(end_date, '%Y-%m-%d'))
             )
-        elif month: 
-            current_year = datetime.now().year
-            query = query.filter(
-                and_(
-                    func.MONTH(Attendance.date) == int(month),
-                    func.YEAR(Attendance.date) == current_year
-                )
-            )
-        if year and not month:
-            query = query.filter(func.YEAR(Attendance.date) == int(year))
+        elif start_date:
+            query = query.filter(Attendance.date >= datetime.strptime(start_date, '%Y-%m-%d'))
+        elif end_date:
+            query = query.filter(Attendance.date <= datetime.strptime(end_date, '%Y-%m-%d'))
 
-        if quarter:
-            quarter_months = {
-                1: [1, 2, 3],
-                2: [4, 5, 6],
-                3: [7, 8, 9],
-                4: [10, 11, 12]
-            }
-            query = query.filter(func.MONTH(Attendance.date).in_(quarter_months.get(int(quarter), [])))
         attendance_data = query.all()
+
         if not attendance_data:
             return jsonify({'error': 'No attendance records found.'}), 404
+
         late_employees = len([a for a in attendance_data if a.time_in and (a.time_in.hour > 8 or (a.time_in.hour == 8 and a.time_in.minute > 30))])
         working_employees = len([a for a in attendance_data if a.time_in])
         absent_employees = len([a for a in attendance_data if not a.time_in])
-        attendance_list = []
-        for attendance in attendance_data:
-            attendance_list.append({
-                'id': attendance.id_attendance,
-                'date': attendance.date.strftime('%Y-%m-%d'),
-                'time_in': attendance.time_in.strftime('%H:%M:%S') if attendance.time_in else None,
-                'time_out': attendance.time_out.strftime('%H:%M:%S') if attendance.time_out else None,
-            })
+
+        attendance_list = [{
+            'id': attendance.id_attendance,
+            'date': attendance.date.strftime('%Y-%m-%d'),
+            'time_in': attendance.time_in.strftime('%H:%M:%S') if attendance.time_in else None,
+            'time_out': attendance.time_out.strftime('%H:%M:%S') if attendance.time_out else None,
+        } for attendance in attendance_data]
 
         return jsonify({
             'summary': {
@@ -60,11 +43,13 @@ def get_employee_attendance_summary(employee_id, day=None, month=None, quarter=N
             },
             "attendance_data": attendance_list
         }), 200
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+
 # admin
-def get_admin_attendance_summary(id=None, day=None, month=None, quarter=None, year=None):
+def get_admin_attendance_summary(id=None, start_date=None, end_date=None):
     current_user_id = get_jwt_identity()
     current_user = Employee.query.get(current_user_id)
     if not current_user or not current_user.isAdmin:
@@ -76,55 +61,34 @@ def get_admin_attendance_summary(id=None, day=None, month=None, quarter=None, ye
         if id:
             query = query.filter(Attendance.id_employee == id)
 
-        if day:
-            query = query.filter(func.DAY(Attendance.date) == int(day))
-
-        if month and year:
+        if start_date and end_date:
             query = query.filter(
-                and_(
-                    func.MONTH(Attendance.date) == int(month),
-                    func.YEAR(Attendance.date) == int(year)
-                )
+                Attendance.date.between(datetime.strptime(start_date, '%Y-%m-%d'), 
+                                        datetime.strptime(end_date, '%Y-%m-%d'))
             )
-        elif month:
-            current_year = datetime.now().year
-            query = query.filter(
-                and_(
-                    func.MONTH(Attendance.date) == int(month),
-                    func.YEAR(Attendance.date) == current_year
-                )
-            )
-        
-        if year and not month:
-            query = query.filter(func.YEAR(Attendance.date) == int(year))
+        elif start_date:
+            query = query.filter(Attendance.date >= datetime.strptime(start_date, '%Y-%m-%d'))
+        elif end_date:
+            query = query.filter(Attendance.date <= datetime.strptime(end_date, '%Y-%m-%d'))
 
-        # Lọc theo quý
-        if quarter:
-            quarter_months = {
-                1: [1, 2, 3],
-                2: [4, 5, 6],
-                3: [7, 8, 9],
-                4: [10, 11, 12]
-            }
-            query = query.filter(func.MONTH(Attendance.date).in_(quarter_months.get(int(quarter), [])))
         attendance_data = query.all()
 
         if not attendance_data:
             return jsonify({'error': 'No attendance records found.'}), 404
+
         total_employees = len(attendance_data)
         late_employees = len([a for a, e in attendance_data if a.time_in and (a.time_in.hour > 8 or (a.time_in.hour == 8 and a.time_in.minute > 30))])
         working_employees = len([a for a, e in attendance_data if a.time_in])
         absent_employees = len([a for a, e in attendance_data if not a.time_in])
-        attendance_list = []
-        for attendance, employee in attendance_data:
-            attendance_list.append({
-                'id': attendance.id_attendance,
-                'id_employee': attendance.id_employee,
-                'employee_name': employee.name if employee else 'Unknown',  
-                'date': attendance.date.strftime('%Y-%m-%d'),
-                'time_in': attendance.time_in.strftime('%H:%M:%S') if attendance.time_in else None,
-                'time_out': attendance.time_out.strftime('%H:%M:%S') if attendance.time_out else None,
-            })
+
+        attendance_list = [{
+            'id': attendance.id_attendance,
+            'id_employee': attendance.id_employee,
+            'employee_name': employee.name if employee else 'Unknown',  
+            'date': attendance.date.strftime('%Y-%m-%d'),
+            'time_in': attendance.time_in.strftime('%H:%M:%S') if attendance.time_in else None,
+            'time_out': attendance.time_out.strftime('%H:%M:%S') if attendance.time_out else None,
+        } for attendance, employee in attendance_data]
 
         return jsonify({
             'summary': {
